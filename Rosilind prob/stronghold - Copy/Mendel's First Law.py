bb92635d05408aa0520c8389d89b767b10dbd5cc
leostein1234 <63097193+leostein1234@users.noreply.github.com>
2020-04-03 20:12:36 -0700

    Delete Mendel's First Law.py

--- Rosilind prob/stronghold - Copy/Mendel's First Law.py
+++
@@ -1,9 +0,0 @@
-def percentDomAllele(x,y,z):
-    total = x + y + z
-    zz = (z/total) * ((z-1)/(total-1))
-    rh = (z/total) * (y/(total -1)) + (y/total) * (z/(total -1))
-    hh = (y/total) * ((y-1)/(total-1))
-    probDom = 1- (zz + hh/4 + rh/2)
-    return probDom
-
-print(percentDomAllele(18,28,20))
